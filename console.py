62eeb78f628a8ba302e54277a2392ea862ad8e8a
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-06-30 20:11:15 +0000

    Update: console.py

--- console.py
+++ console.py
@@ -11,9 +11,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
 allclases = {"BaseModel": BaseModel, "User": User, "State": State,
-    "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+             "City": City, "Amenity": Amenity,
+             "Place": Place, "Review": Review}
+
 
 def errors(arg):
     """This function check if the input data is complete
@@ -46,20 +47,29 @@ def parsing(var):
     else:
         return float(var)
 
+
 def count(arg):
-        a = storage.all()
-        b = 0
-        for i in a.keys():
-            c = str(i)
-            c = c.split(".")
-            if arg == c[0]:
-                b += 1
-        return b
+    """This funtion count the number of instaces of a class
+        Args:
+            - arg : Class to count instances"""
+    a = storage.all()
+    b = 0
+    for i in a.keys():
+        c = str(i)
+        c = c.split(".")
+        if arg == c[0]:
+            b += 1
+    return b
+
 
 def advance(self, arg, cls):
+    """This funtion choose the property method for <cls>.<method>.()
+    Args:
+        -arg : string with method, id, instance name and instance value
+        -cls : class to apply the method"""
     if len(arg) != 0:
         if arg == ".all()":
-            self.do_all(cls) 
+            self.do_all(cls)
         elif arg == ".count()":
             print(count(cls))
         elif arg[0:9] == ".destroy(":
@@ -71,10 +81,35 @@ def advance(self, arg, cls):
         elif arg[0:8] == ".update(":
             c = arg.split("(")
             d = c[1].split(", ")
-            print(cls + " " + d[0][1:-1] + " " + d[1][1:-1]
-                + " " + d[2][:-1])
-            self.do_update(cls + " " + d[0][1:-1] + " " + d[1][1:-1]
-                + " " + d[2][:-1])
+            self.do_update(cls + " " + d[0][1:-1] + " " + d[1] +
+                           " " + d[2][:-1])
+
+
+def split_string(s, lim):
+    """This funtion create and return a list dividing a string using a pattern
+    Args:
+        -s : String to divide
+        -lim : String parttern to divide s
+    Return:
+        -List of strings"""
+    a = []
+    b = ""
+    c = 0
+    d = 0
+    for i in range(len(s)):
+        if s[i] != lim:
+            b += s[i]
+        else:
+            a.append(b)
+            b = ""
+    a.append(b)
+    for j in range(3, len(a)):
+        if a[j][0] == '"' and a[j][len(a[j]) - 1] != '"':
+            c = j
+        elif a[j][0] != '"' and a[j][len(a[j]) - 1] == '"':
+            d = j
+    a[c] = " ".join(a[c:d + 1])
+    return a
 
 
 class HBNBCommand(cmd.Cmd):
@@ -173,7 +208,6 @@ class HBNBCommand(cmd.Cmd):
                     b.append(a[i].__str__())
             print(b)
 
-
     def do_update(self, arg):
         """This funtion updates an instance based on the class name
         and id by adding or updating attribute (save the change into
@@ -183,7 +217,7 @@ class HBNBCommand(cmd.Cmd):
             - id
             - Attribute name
             - Attribute value"""
-        var = arg.split()
+        var = split_string(arg, " ")
         k = errors(var)
         if k != 2:
             z = 0
@@ -212,30 +246,45 @@ class HBNBCommand(cmd.Cmd):
             except:
                 ValueError
 
-
     def do_User(self, arg):
+        """Method to call User in the form User.<method>()
+        Args:
+            - string from console"""
         if len(arg) != 0:
             advance(self, arg, "User")
-        #Falta error
 
-    
     def do_City(self, arg):
+        """Method to call Cityin the form City.<method>()
+        Args:
+            - string from console"""
         if len(arg) != 0:
             advance(self, arg, "City")
-    
+
     def do_BaseModel(self, arg):
+        """Method to call Amenity in the form Amenity.<method>()
+        Args:
+            - String from console"""
         if len(arg) != 0:
             advance(self, arg, "BaseModel")
-    
+
     def do_Review(self, arg):
+        """Method to call Review in the form Review.<method>()
+        Args:
+            - String from console"""
         if len(arg) != 0:
             advance(self, arg, "Review")
-    
+
     def do_Amenity(self, arg):
+        """Method to call Amenity in the form Amenity.<method>()
+        Args:
+            - String from console"""
         if len(arg) != 0:
             advance(self, arg, "Amenity")
 
     def do_State(self, arg):
+        """Method to call State in the form State.<method>()
+        Args:
+            - string from console"""
         if len(arg) != 0:
             advance(self, arg, "State")
 
